e3b65790df76b81a11ff41c10ab325b4bb88ee9b
Sean O'Hara <sohara@sohara.com>
2020-03-10 22:47:35 -0400

    Add ability to register a user

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
 import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
 import * as express from "express";
-import { buildSchema, Resolver, Query } from "type-graphql";
-
-@Resolver()
-class HelloResolver {
-  @Query(() => String, { nullable: true })
-  async hello() {
-    return "hell yeah!";
-  }
-}
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
+import { RegisterResolver } from "./modules/user/Register";
 
 const main = async () => {
+  await createConnection();
   const schema = await buildSchema({
-    resolvers: [HelloResolver]
+    resolvers: [RegisterResolver]
   });
   const apolloServer = new ApolloServer({ schema });
   const app = express();
---
+++ src/modules/user/Register.ts
@@ -0,0 +1,40 @@
+import {
+  Resolver,
+  Mutation,
+  Query,
+  Arg,
+  FieldResolver,
+  Root
+} from "type-graphql";
+import * as bcrypt from "bcrypt";
+import { User } from "../../entities/User";
+
+@Resolver(User)
+export class RegisterResolver {
+  @Query(() => String)
+  async hello() {
+    return "hell yeah!";
+  }
+  @FieldResolver()
+  async name(@Root() parent: User) {
+    return `${parent.firstName} ${parent.lastName}`;
+  }
+
+  @Mutation(() => User)
+  async register(
+    @Arg("firstName") firstName: string,
+    @Arg("lastName") lastName: string,
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword
+    }).save();
+
+    return user;
+  }
+}
